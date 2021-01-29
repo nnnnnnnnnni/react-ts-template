5cc5259e7840e7951d35cf29ca9014d964b703a0
nnnnnnnnnni <714275326@qq.com>
2021-01-29 18:10:33 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,16 @@ import index from "./index.css";
 
 ReactDom.render(
   <div className={index.container}>
-    <div className={index.header}>React-template</div>
-    <a className={index.author} target='__black' href="https://github.com/nnnnnnnnnni">author: shenger</a>
+    <div className={index.header}>react-template</div>
+    <div className={index.link}>
+      <a className={index.author} target="__black" href="https://weibo.com/u/5462337214">
+        <img src='../public/github.png' alt=""/>
+        一个男孩子-</a>
+      <a className={index.author} target="__black" href="https://github.com/nnnnnnnnnni">
+        <img src='../public/weibo.png' alt=""/>
+        shenger
+      </a>
+    </div>
   </div>,
   document.getElementById("root")
 );
